cd79cc89681491dc9666524cb2fa7c17da51edc9
Xuhaijing <63711186+XuHaijing-1@users.noreply.github.com>
2020-04-24 23:48:30 +0800

    0006 增加启动页面

---
+++ app/src/main/java/com/example/diandian/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.example.diandian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView img;
+
+    // private final int SPLASH_DISPLAY_LENGHT = 2000; // 两秒后进入系统
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_splash);
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                try{
+                    sleep(2000);//使程序休眠五秒
+                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+
+        img=findViewById(R.id.img);
+
+        img.setOnClickListener((v)->{
+            //开始透明度动画
+            Animation alpha= AnimationUtils.loadAnimation(this,
+                    R.anim.alpha_animation);
+            img.startAnimation(alpha);
+        });
+    }
+}
